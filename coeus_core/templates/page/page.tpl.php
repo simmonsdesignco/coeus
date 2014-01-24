48b91ba8736eb75078741f097d72a30dfdc25923
Richard Buchanan <richard_buchanan@buchanandesigngroup.com>
2014-01-23 21:37:37 -0500

    Edited page.tpl.php variables explenations

--- coeus_core/templates/page/page.tpl.php
+++ coeus_core/templates/page/page.tpl.php
@@ -5,7 +5,7 @@
  * Default theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
- * can be found in the html.tpl.php template in the ../templates directory.
+ * can be found in the html.tpl.php template in the ./templates directory.
  *
  * Available variables:
  *
